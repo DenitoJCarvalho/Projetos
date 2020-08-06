cd3fe5683d097187128a61aeba94276f3535bab9
DennisJCarvalho <hdtecitu@gmail.com>
2020-08-06 20:06:50 -0300

    Complementando o formulario

--- next level week/2/web/src/pages/TeacherForm/index.tsx
+++ next level week/2/web/src/pages/TeacherForm/index.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
 import Input from '../../components/input'
+import Textarea from '../../components/TextArea'
+import Select from '../../components/Select'
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css'
+
 
 
 function TeacherForm() {
@@ -23,14 +26,63 @@ function TeacherForm() {
                     <Input name="avatar" label="Avatar" />
                     <Input name="whatsapp" label="Whatsapp" />
 
+                    <Textarea name="bio" label="Biografia" />
+
                 </fieldset>
 
                 <fieldset>
                     <legend> Sobre a aula</legend>
                     
-                    <Input name="subject" label="Nome completo" />
-                    <Input name="cost" label="Avatar" />
+                    <Select 
+                        name="subject" 
+                        label="Nome completo" 
+                        options={[
+                            { value: 'Artes',label: 'Artes' },
+                            { value: 'Biologia',label: 'Biologia' },
+                            { value: 'Ciências',label: 'Ciências' },
+                            { value: 'Educação Física',label: 'Educação Física' },
+                            { value: 'Física',label: 'Física' },
+                            { value: 'Geografia',label: 'Geografia' },
+                            { value: 'História',label: 'História' },
+                            { value: 'Matemática',label: 'Matemática' },
+                            { value: 'Portugês',label: 'Português' },
+                            { value: 'Química',label: 'Química' },
+                            { value: 'Inglês',label: 'Inglês' },
+                            { value: 'Espanhol',label: 'Espanhol' },
+                            { value: 'Linguagem e Alfabetização',label: 'Linguagem e Alfabetização' },
+                            { value: 'História/Geografia',label: 'História/Geografia' },
+                            { value: 'Ética e Cidadania',label: 'Ética e Cidadania' },
+                            { value: 'Robótica',label: 'Robótica' },
+                        ]}
+                    />
 
+                    <Input name="cost" label="Custo da hora por aula." />
+                </fieldset>
+
+                <fieldset>
+                        <legend>
+                            Horários disponíveis
+                            <button type="button"> + Novo horário </button>
+                        </legend>
+
+                        <div className="schedule-item">
+                            <Select 
+                                name="week_day" 
+                                label="Dia da semana" 
+                                options={[
+                                    { value: '0',label: 'Domingo' },
+                                    { value: '1',label: 'Segunda-Feira' },
+                                    { value: '2',label: 'Terça-Feira' },
+                                    { value: '3',label: 'Quarta-Feira' },
+                                    { value: '4',label: 'Quinta-Feira' },
+                                    { value: '5',label: 'Sexta-Feira' },
+                                    { value: '6',label: 'Sábado' }
+                                ]}
+                            />
+
+                            <Input name="from" label="Das" type="time" />
+                            <Input name="to" label="até" type="time" />
+                        </div>
                 </fieldset>
 
                 <footer>
--- next level week/2/web/src/pages/TeacherList/index.tsx
+++ next level week/2/web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
 import Input from '../../components/input'
+import Select from '../../components/Select'
 
 import './styles.css'
 
@@ -14,7 +15,43 @@ function TeacherList(){
         <div id="page-teacher-list" className="container">
             <PageHeader title="Que incrível que você quer dar aulas.">
                 <form id="search-teachers">
-                    <Input name="subject" label="Matéria" />
+                <Select 
+                        name="subject" 
+                        label="Nome completo" 
+                        options={[
+                            { value: 'Artes',label: 'Artes' },
+                            { value: 'Biologia',label: 'Biologia' },
+                            { value: 'Ciências',label: 'Ciências' },
+                            { value: 'Educação Física',label: 'Educação Física' },
+                            { value: 'Física',label: 'Física' },
+                            { value: 'Geografia',label: 'Geografia' },
+                            { value: 'História',label: 'História' },
+                            { value: 'Matemática',label: 'Matemática' },
+                            { value: 'Portugês',label: 'Português' },
+                            { value: 'Química',label: 'Química' },
+                            { value: 'Inglês',label: 'Inglês' },
+                            { value: 'Espanhol',label: 'Espanhol' },
+                            { value: 'Linguagem e Alfabetização',label: 'Linguagem e Alfabetização' },
+                            { value: 'História/Geografia',label: 'História/Geografia' },
+                            { value: 'Ética e Cidadania',label: 'Ética e Cidadania' },
+                            { value: 'Robótica',label: 'Robótica' },
+                        ]}
+                    />
+
+                    <Select 
+                        name="week_day" 
+                        label="Dia da semana" 
+                        options={[
+                            { value: '0',label: 'Domingo' },
+                            { value: '1',label: 'Segunda-Feira' },
+                            { value: '2',label: 'Terça-Feira' },
+                            { value: '3',label: 'Quarta-Feira' },
+                            { value: '4',label: 'Quinta-Feira' },
+                            { value: '5',label: 'Sexta-Feira' },
+                            { value: '6',label: 'Sábado' }
+                        ]}
+                    />
+
                     <Input name="week_day" label="Dia da semana" />
                     <Input type="time" name="time" label="Hora" /> 
                 </form>
