d49efc2f55faaa627cac175f4578ea2fcb861abb
DennisJCarvalho <hdetcitu@gmail.com>
2020-08-24 13:04:25 -0300

    inserido alguns componentes da página principal

--- manager mei/1.0.0/web/src/components/Box/index.js
+++ manager mei/1.0.0/web/src/components/Box/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
+
 import Header from '../Header'
+import ContentMain from '../ContentMain'
 
 import './style.css'
 
@@ -9,6 +11,30 @@ const Box = () => {
     return (
         <section className="box">
             <Header />
+            <ContentMain 
+                title="Revenda de Mercadoria"
+                label1="C/ nota fiscal"
+                label2="S/ nota fiscal"
+                label3="Total das revendas"
+                nameButton="Inserir"    
+            />
+
+            <ContentMain 
+                title="Venda de Produtos Industrializados"
+                label1="C/ nota fiscal"
+                label2="S/ nota fiscal"
+                label3="Total das vendas"
+                nameButton="Inserir"    
+            />
+
+            <ContentMain 
+                title="Prestação de Serviço"
+                label1="C/ nota fiscal"
+                label2="S/ nota fiscal"
+                label3="Total prest.serviço"    
+                nameButton="Inserir"
+            />
+
         </section>
     )
 }
---
+++ manager mei/1.0.0/web/src/components/ContentMain/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+import './style.css'
+
+const ContentMain = (props) => {
+
+    const valueDefault = '0,00'
+
+    return (
+        <section className="container-content-main">
+            <h3>{props.title}</h3>
+            
+            <label>{props.label1}</label>
+            <div>{`R$${valueDefault}`}</div>
+            <label>{props.label2}</label>
+            <div>{`R$${valueDefault}`}</div>
+            <label>{props.label3}</label>
+            <div>{`R$${valueDefault}`}</div>
+
+            <button>{props.nameButton}</button>
+        </section>
+
+    )
+}
+
+export default ContentMain
